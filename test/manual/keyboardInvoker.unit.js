913b073f38cc06d4dfeee6f72c984fb39bf206fb
Fernando Claverino@gmail.com <fernando.claverino@gmail.com>
2016-12-09 17:26:55 -0300

    add manual test to keyboard invoker

---
+++ test/manual/keyboardInvoker.unit.js
@@ -0,0 +1,23 @@
+const KeyboardInvoker = require('../../car/keyboardInvoker');
+
+const commands = {
+  'q': () => process.exit(),
+  'up': () => {
+    console.log('up');
+  },
+  'down': () => {
+    console.log('down');
+  },
+  'left': () => {
+    console.log('left');
+  },
+  'right': () => {
+    console.log('right');
+  },
+  'space': () => {
+    console.log('space');
+  }
+};
+const keyboardInvoker = new KeyboardInvoker(commands);
+
+keyboardInvoker.listen();
