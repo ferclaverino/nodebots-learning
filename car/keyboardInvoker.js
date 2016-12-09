38e5c53678765d608c55dcb272ee402ad522a66b
Fernando Claverino@gmail.com <fernando.claverino@gmail.com>
2016-12-09 18:28:03 -0300

    allow keyboard invoker to start and stop

--- car/keyboardInvoker.js
+++ car/keyboardInvoker.js
@@ -1,15 +1,38 @@
 const Keyboard = require('node-keyboard');
 
+function mapKeyName(keyId) {
+  return keyId.replace('KEY_', '').toLowerCase();
+}
+
 class KeyboardInvoker {
+
   constructor(invoker, event) {
     this.keyboard = new Keyboard(event);
     this.invoker = invoker;
+    this.lastEvent = {
+      event: null,
+      key: null
+    };
+  }
+
+  executeCommand(event, key, command) {
+    if (this.lastEvent.event !== event && this.lastEvent.key !== key) {
+      this.lastEvent = {
+        key: key,
+        event: event
+      };
+      var keyName = mapKeyName(key.keyId);
+      command(keyName);
+    }
   }
 
   listen() {
-    this.keyboard.on('keypress', (key) => {
-      var keyName = key.keyId.replace('KEY_', '').toLowerCase();
-      this.invoker.start(keyName);
+    this.keyboard.on('keydown', (key) => {
+      this.executeCommand('keydown', key, (command) => this.invoker.start(command))
+    });
+
+    this.keyboard.on('keyup', (key) => {
+      this.executeCommand('keyup', key, (command) => this.invoker.stop(command))
     });
   }
 
--- test/manual/keyboardInvoker.unit.js
+++ test/manual/keyboardInvoker.unit.js
@@ -3,19 +3,20 @@ const KeyboardInvoker = require('../../car/keyboardInvoker');
 
 const commands = {
   'up': {
-    start: () => console.log('start up')
+    start: () => console.log('start up'),
+    stop: () => console.log('stop up')
   },
   'down': {
-    start: () => console.log('start down')
+    start: () => console.log('start down'),
+    stop: () => console.log('stop down')
   },
   'left': {
-    start: () => console.log('start left')
+    start: () => console.log('start left'),
+    stop: () => console.log('stop left')
   },
   'right': {
-    start: () => console.log('start right')
-  },
-  'space': {
-    start: () => console.log('start space')
+    start: () => console.log('start right'),
+    stop: () => console.log('stop right')
   }
 };
 var invoker = new Invoker(commands);
